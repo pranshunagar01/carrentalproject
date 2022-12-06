fdee1b7e87a0aad0c73a67d6b7bc5f39f9ba02f4
pranshunagar01 <59359937+pranshunagar01@users.noreply.github.com>
2022-12-06 09:05:26 -0600

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const corsOptions ={
 app.use(cors(corsOptions));
 
 app.use(function(req,res,next){
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
     res.header("Access-Control-Allow-Credentials", true);

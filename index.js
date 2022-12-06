f1cc07c307237d366cc36edd3e379e76b9dd7e1b
pranshunagar01 <59359937+pranshunagar01@users.noreply.github.com>
2022-12-06 02:45:39 -0600

    Update index.js

--- index.js
+++ index.js
@@ -24,11 +24,19 @@ con.connect(function(err) {
 
 app.use(bodyParser.urlencoded({ extended: false }));
 const corsOptions ={
-    origin:'http://localhost:3000/', 
+    origin:'http://localhost:3000', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
 app.use(cors(corsOptions));
+
+app.use(function(req,res,next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+});
 app.use(express.json());
 
 
